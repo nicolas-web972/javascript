5c4aa7a78da75f841cbe6204b7156f62e64fa5bb
Nicolas Chalono <nicolas.chalono@gmail.com>
2021-12-01 10:53:23 +0100

    challenge12

---
+++ challenge_12/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const temperatures1 = [17, 21, 23];
+
+for (let i = 0; i < temperatures1.length; i++) {
+  console.log(`${temperatures1[i]} degrés dans ${i + 1} jour(s).`);
+};
+
+const temperatures2 = [12.5, 5, -5, 0, 4];
+
+for (let i = 0; i < temperatures2.length; i++) {
+  console.log(`${temperatures2[i]} degrés dans ${i + 1} jour(s).`);
+};
